58f385e357c2934e2b47f686a1e2a5b763e61af9
Ido Silberstein <idosilberstein@gmail.com>
2026-09-08 14:03:03 +0300

    fix: use os.getenv in src/config/settings.py

--- src/config/settings.py
+++ src/config/settings.py
@@ -1,6 +1,8 @@
-from dotenv import load_dotenv
 import os
+from dotenv import load_dotenv
 
 load_dotenv()
 
-ANTHROPIC_API_KEY = os.environ.get("ANTHROPIC_API_KEY")
+ANTHROPIC_API_KEY: str = os.getenv("ANTHROPIC_API_KEY", "")
+OPENAI_API_KEY: str = os.getenv("OPENAI_API_KEY", "")
+GOOGLE_API_KEY: str = os.getenv("GOOGLE_API_KEY", "")
